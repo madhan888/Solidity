320e0b457bdea3637dd3ea1df9c70714627bee4d
Remix Workspace <>
2022-08-23 17:44:14 +0530

    commit

---
+++ rough.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.5.0 <0.9.0;
+
+contract FunctionArgument{
+          
+          function find(int a,int b, int c) public pure returs(int){
+
+              return ((a+b+c)/3
+              )
+          }
